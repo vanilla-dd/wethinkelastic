9446a6643566f098f08bc5cd5320ef69538a5b67
vanilla-dd <akashsingh123456765432@gmail.com>
2024-11-22 14:21:47 +0530

    chore: og tags

--- src/main.ts
+++ src/main.ts
@@ -3,3 +3,6 @@ import "./style.css";
 import { ScrollTrigger } from "gsap/all";
 
 gsap.registerPlugin(ScrollTrigger);
+window.addEventListener("load", () => {
+  console.log("Page is fully loaded!");
+});
