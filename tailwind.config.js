a442a0e689aa1e23e6b5eb0b0973dc4d79ffc746
vanilla-dd <akashsingh123456765432@gmail.com>
2024-11-22 11:17:20 +0530

    chore: fonts install

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        ivypresto: ["ivypresto", "serif"],
+        helveticaneue: ["helveticaneue", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
